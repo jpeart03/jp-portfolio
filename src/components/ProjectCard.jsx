7a9eceada5521b70602bee0369bde1d42c9a0ed5
jpeart03 <james.r.peart@gmail.com>
2021-07-28 16:13:18 -0500

    initial commit

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,10 @@
+function ProjectCard(props) {
+    return (
+        <div>
+            Hey, I'm a card!
+            This prop was passed in: {props.astro}
+        </div>
+    )
+}
+
+export default ProjectCard
